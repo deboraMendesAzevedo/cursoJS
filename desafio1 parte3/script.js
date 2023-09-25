d952d3a906cd01e6a28ab3b0284cd9afdb5969f5
Débora Mendes de Azevedo <86240362+deboraMendesAzevedo@users.noreply.github.com>
2023-09-25 15:51:35 -0300

    inclusão do desafio

---
+++ desafio1 parte3/script.js
@@ -0,0 +1,59 @@
+function verificar(){
+    
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('inascimento')
+    var resposta = document.getElementById('iresposta')
+
+    if(fano.value.length == 0 || fano.value > ano){
+        window.alert('Erro')
+    }
+    else{
+        var sexo = document.getElementsByName('sexo')
+        var idade = ano - Number(fano.value)
+        
+        var genero = ''
+
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+
+        if (sexo[0].checked){
+            genero = 'Homem'
+            if (idade >= 0 && idade >= 10){
+                // criança
+                window.alert('crianca')
+                //img.setAttribute('src' , 'menino.jpg')
+                
+
+            }else if(idade < 21){
+                // jovem
+
+            }else if(idade < 60){
+                // adulto
+
+            }else{
+                // idoso
+
+            }
+
+        }
+        else if (sexo[1].checked){
+            genero = 'Feminino'
+        }
+        if(idade == 0 && idade <10){
+            // criança
+
+        }else if(idade < 21){
+            // jovem
+
+        }else if( idade < 60){
+            // adulto
+
+        }else{
+            // idoso
+
+        }
+        resposta.style.textAlign = 'center'
+        resposta.innerHTML = `Idade calculada ${idade} anos , genero ${genero}`
+    }
+}
