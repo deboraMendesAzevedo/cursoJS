ba061c6e207fc43fa86bc84959744140177929da
Débora Mendes de Azevedo <86240362+deboraMendesAzevedo@users.noreply.github.com>
2023-09-25 16:47:40 -0300

    inclusão aula 14

---
+++ aula014/ambiente.js
@@ -0,0 +1,20 @@
+var i = 1
+
+while(i <= 10){
+    console.log(`${i} while`)
+    i ++
+}
+
+var i = 1
+
+do{
+    console.log(`${i} do`)
+    i ++
+}while(i <=6)
+
+//========================================
+console.log('Vai começar')
+for(var i = 1; i <= 100; i++){
+    console.log(`For ${i}`)
+}
+console.log('Fim')
---
+++ aula014/node_30b95b984db65.tmp.js
@@ -0,0 +1,12 @@
+var i = 1
+
+while(i <= 10){
+    console.log(`${i} while`)
+    i ++
+}
+
+var i = 1
+
+do{
+    console.log(`${i} do`)
+}while(i <=6)
