dac13dfaed16a580d8ae9cb327c108c3bc3ff4dd
Débora Mendes de Azevedo <86240362+deboraMendesAzevedo@users.noreply.github.com>
2023-09-25 16:26:05 -0300

    inclusão da aula13

---
+++ aula013/ambiente.js
@@ -0,0 +1,20 @@
+// console.log('Tudo bem ?')
+
+var i = 1
+while (i <= 6){
+    console.log('Tudo bem ?')
+    i++
+}
+
+var i = 1
+while (i <= 10){
+console.log(`Passo  ${i}`)
+    i++
+}
+
+var i = 1
+do {
+    console.log(`teste ${i}`)
+    i++
+} while (i <= 10)
+
---
+++ aula013/node_dd07e5a1e75d6.tmp.js
@@ -0,0 +1,20 @@
+// console.log('Tudo bem ?')
+
+var i = 1
+while (i <= 6){
+    console.log('Tudo bem ?')
+    i++
+}
+
+var i = 1
+while (i <= 10){
+console.log(`Passo  ${i}`)
+    i++
+}
+
+
+do {
+    console.log(`teste ${i}`)
+    i++
+} while (i >= 5)
+
