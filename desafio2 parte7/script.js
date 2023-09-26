6f0a42cb127a0078c44f8b06221606cc2491b8b1
Débora Mendes de Azevedo <86240362+deboraMendesAzevedo@users.noreply.github.com>
2023-09-26 17:57:23 -0300

    inclusão de ex

---
+++ desafio2 parte7/script.js
@@ -0,0 +1,15 @@
+
+var numero = document.getElementById('inumero')
+var resposta = document.getElementById('')
+
+
+function analisar(x){
+    var posicao = num.indexOf(x)
+    if(x <= 0 && x > 100 ){
+        return ('número invalido')
+
+    }else{
+        var soma = 
+    }
+    
+}
